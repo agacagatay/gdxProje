f6782ffb1c15bc3e595e50baf2c13e68baecc021
agacagatay <ahmetce06@gmail.com>
2014-07-15 17:37:31 +0300

    1st

---
+++ OyunTest/desktop/src/com/taylak/oyuntest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.taylak.oyuntest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.taylak.oyuntest.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
