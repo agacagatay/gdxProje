f65e5c8b09a286d581a29287540a82fbdaedf805
agacagatay <ahmetce06@gmail.com>
2014-08-06 17:23:40 +0300

    iş

--- grafikod/core/src/grafikod/ders/view/WorldRenderer.java
+++ grafikod/core/src/grafikod/ders/view/WorldRenderer.java
@@ -1,6 +1,5 @@
 package grafikod.ders.view;
 
- 
 import grafikod.ders.model.Block;
 import grafikod.ders.model.Bob;
 import grafikod.ders.model.Bob.State;
@@ -22,7 +21,7 @@ public class WorldRenderer {
 	private static final float CAMERA_WIDTH = 10f;
 	private static final float CAMERA_HEIGHT = 7f;
 	private static final float RUNNING_FRAME_DURATION = 0.06f;
-	
+
 	private World world;
 	private OrthographicCamera cam;
 
@@ -38,44 +37,48 @@ public class WorldRenderer {
 	private TextureRegion bobFallLeft;
 	private TextureRegion bobJumpRight;
 	private TextureRegion bobFallRight;
-	
+
 	/** Animations **/
 	private Animation walkLeftAnimation;
 	private Animation walkRightAnimation;
-	
+
 	private SpriteBatch spriteBatch;
 	private boolean debug = false;
 	private int width;
 	private int height;
-	private float ppuX;	// pixels per unit on the X axis
-	private float ppuY;	// pixels per unit on the Y axis
-	
-	public void setSize (int w, int h) {
+	private float ppuX; // pixels per unit on the X axis
+	private float ppuY; // pixels per unit on the Y axis
+
+	public void setSize(int w, int h) {
 		this.width = w;
 		this.height = h;
-		ppuX = (float)width / CAMERA_WIDTH;
-		ppuY = (float)height / CAMERA_HEIGHT;
+		ppuX = (float) width / CAMERA_WIDTH;
+		ppuY = (float) height / CAMERA_HEIGHT;
 	}
+
 	public boolean isDebug() {
 		return debug;
 	}
+
 	public void setDebug(boolean debug) {
 		this.debug = debug;
 	}
 
 	public WorldRenderer(World world, boolean debug) {
 		this.world = world;
-		this.cam = new OrthographicCamera(CAMERA_WIDTH*3, CAMERA_HEIGHT*3);
-        this.cam.position.set(world.getBob().getPosition().x, world.getBob().getPosition().y, 0);
-//		this.cam.position.set(CAMERA_WIDTH / 2f, CAMERA_HEIGHT / 2f, 0);
+		this.cam = new OrthographicCamera(CAMERA_WIDTH*2, CAMERA_HEIGHT*2);
+		this.cam.position.set(world.getBob().getPosition().x, world.getBob()
+				.getPosition().y, 0);
+		// this.cam.position.set(CAMERA_WIDTH / 2f, CAMERA_HEIGHT / 2f, 0);
 		this.cam.update();
 		this.debug = debug;
 		spriteBatch = new SpriteBatch();
 		loadTextures();
 	}
-	
+
 	private void loadTextures() {
-		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("images/textures/textures.pack"));
+		TextureAtlas atlas = new TextureAtlas(
+				Gdx.files.internal("images/textures/textures.pack"));
 		bobIdleLeft = atlas.findRegion("bob-01");
 		bobIdleRight = new TextureRegion(bobIdleLeft);
 		bobIdleRight.flip(true, false);
@@ -84,7 +87,8 @@ public class WorldRenderer {
 		for (int i = 0; i < 5; i++) {
 			walkLeftFrames[i] = atlas.findRegion("bob-0" + (i + 2));
 		}
-		walkLeftAnimation = new Animation(RUNNING_FRAME_DURATION, walkLeftFrames);
+		walkLeftAnimation = new Animation(RUNNING_FRAME_DURATION,
+				walkLeftFrames);
 
 		TextureRegion[] walkRightFrames = new TextureRegion[5];
 
@@ -92,7 +96,8 @@ public class WorldRenderer {
 			walkRightFrames[i] = new TextureRegion(walkLeftFrames[i]);
 			walkRightFrames[i].flip(true, false);
 		}
-		walkRightAnimation = new Animation(RUNNING_FRAME_DURATION, walkRightFrames);
+		walkRightAnimation = new Animation(RUNNING_FRAME_DURATION,
+				walkRightFrames);
 		bobJumpLeft = atlas.findRegion("bob-up");
 		bobJumpRight = new TextureRegion(bobJumpLeft);
 		bobJumpRight.flip(true, false);
@@ -100,33 +105,43 @@ public class WorldRenderer {
 		bobFallRight = new TextureRegion(bobFallLeft);
 		bobFallRight.flip(true, false);
 	}
-	
-	
+
 	public void render() {
-        spriteBatch.setProjectionMatrix(cam.combined);
+
+		this.cam.position.set(world.getBob().getPosition().x, world.getBob()
+				.getPosition().y, 0);
+		cam.update();
+
+		spriteBatch.setProjectionMatrix(cam.combined);
 		spriteBatch.begin();
-			drawBlocks();
-			drawBob();
+		drawBlocks();
+		drawBob();
 		spriteBatch.end();
+
 		drawCollisionBlocks();
 		if (debug)
 			drawDebug();
-		
+
 	}
 
-
 	private void drawBlocks() {
-		for (Block block : world.getDrawableBlocks((int)CAMERA_WIDTH, (int)CAMERA_HEIGHT)) {
-//            spriteBatch.draw(blockTexture, block.getPosition().x * ppuX, block.getPosition().y * ppuY, Block.SIZE * ppuX, Block.SIZE * ppuY);
-            spriteBatch.draw(blockTexture, block.getPosition().x, block.getPosition().y, Block.SIZE, Block.SIZE);
+		for (Block block : world.getDrawableBlocks((int) CAMERA_WIDTH,
+				(int) CAMERA_HEIGHT)) {
+			// spriteBatch.draw(blockTexture, block.getPosition().x * ppuX,
+			// block.getPosition().y * ppuY, Block.SIZE * ppuX, Block.SIZE *
+			// ppuY);
+			spriteBatch.draw(blockTexture, block.getPosition().x,
+					block.getPosition().y, Block.SIZE, Block.SIZE);
 		}
 	}
 
 	private void drawBob() {
 		Bob bob = world.getBob();
 		bobFrame = bob.isFacingLeft() ? bobIdleLeft : bobIdleRight;
-		if(bob.getState().equals(State.WALKING)) {
-			bobFrame = bob.isFacingLeft() ? walkLeftAnimation.getKeyFrame(bob.getStateTime(), true) : walkRightAnimation.getKeyFrame(bob.getStateTime(), true);
+		if (bob.getState().equals(State.WALKING)) {
+			bobFrame = bob.isFacingLeft() ? walkLeftAnimation.getKeyFrame(
+					bob.getStateTime(), true) : walkRightAnimation.getKeyFrame(
+					bob.getStateTime(), true);
 		} else if (bob.getState().equals(State.JUMPING)) {
 			if (bob.getVelocity().y > 0) {
 				bobFrame = bob.isFacingLeft() ? bobJumpLeft : bobJumpRight;
@@ -134,15 +149,18 @@ public class WorldRenderer {
 				bobFrame = bob.isFacingLeft() ? bobFallLeft : bobFallRight;
 			}
 		}
-//        spriteBatch.draw(bobFrame, bob.getPosition().x * ppuX, bob.getPosition().y * ppuY, Bob.SIZE * ppuX, Bob.SIZE * ppuY);
-        spriteBatch.draw(bobFrame, bob.getPosition().x, bob.getPosition().y, Bob.SIZE, Bob.SIZE);
+		// spriteBatch.draw(bobFrame, bob.getPosition().x * ppuX,
+		// bob.getPosition().y * ppuY, Bob.SIZE * ppuX, Bob.SIZE * ppuY);
+		spriteBatch.draw(bobFrame, bob.getPosition().x, bob.getPosition().y,
+				Bob.SIZE, Bob.SIZE);
 	}
 
 	private void drawDebug() {
 		// render blocks
 		debugRenderer.setProjectionMatrix(cam.combined);
 		debugRenderer.begin(ShapeType.Line);
-		for (Block block : world.getDrawableBlocks((int)CAMERA_WIDTH, (int)CAMERA_HEIGHT)) {
+		for (Block block : world.getDrawableBlocks((int) CAMERA_WIDTH,
+				(int) CAMERA_HEIGHT)) {
 			Rectangle rect = block.getBounds();
 			debugRenderer.setColor(new Color(1, 0, 0, 1));
 			debugRenderer.rect(rect.x, rect.y, rect.width, rect.height);
@@ -154,15 +172,16 @@ public class WorldRenderer {
 		debugRenderer.rect(rect.x, rect.y, rect.width, rect.height);
 		debugRenderer.end();
 	}
-	
+
 	private void drawCollisionBlocks() {
 		debugRenderer.setProjectionMatrix(cam.combined);
 		debugRenderer.begin(ShapeType.Filled);
 		debugRenderer.setColor(Color.WHITE);
 		for (Rectangle rect : world.getCollisionRects()) {
-			debugRenderer.rect(rect.x, rect.y, rect.width, rect.height, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE);
+			debugRenderer.rect(rect.x, rect.y, rect.width, rect.height,
+					Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE);
 		}
 		debugRenderer.end();
-		
+
 	}
 }
